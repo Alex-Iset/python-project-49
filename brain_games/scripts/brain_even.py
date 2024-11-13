81cfa655099c443820248e754be5b2dc7c0a8912
Alex Iset <fl3shtrader@yandex.ru>
2024-11-13 20:34:14 +0300

    added brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.game_brain_even import start_game
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    start_game()
+
+
+if __name__ == '__main__':
+    main()
