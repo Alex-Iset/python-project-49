e43b8a7bccc45bac749f186b7e1c37616bad4ae5
Alex Iset <fl3shtrader@yandex.ru>
2024-11-14 12:35:14 +0300

    fixed the logic game_brain_even.py

--- brain_games/game_brain_even.py
+++ brain_games/game_brain_even.py
@@ -3,22 +3,26 @@ import prompt
 
 
 def get_random_number() -> int:
+    """Returns a random number in the range from 1 to 100"""
     random_number: int = randint(1, 100)
     return random_number
 
 
 def get_user_answer() -> str:
+    """Returns the user's response"""
     user_answer: str = prompt.string('Your answer: ')
     return user_answer
 
 
 def get_congratulation(user_name: str) -> str:
+    """Congratulates the user"""
     return f'Congratulations, {user_name}!'
 
 
 def check_user_answer(random_number: int,
                       user_name: str,
                       user_answer: str) -> str:
+    """Checks the user's response"""
     if user_answer.lower() == 'yes':
         if random_number % 2 == 0:
             return 'Correct!'
@@ -39,26 +43,19 @@ def check_user_answer(random_number: int,
                 f"\nLet's try again, {user_name}!")
 
 
-def get_user_answers(check: str, user_name: str) -> str:
-    count = 0
-    while count < 2:
-        if check == 'Correct!':
-            count += 1
-            print(f'Question: {get_random_number()}')
-            get_user_answer()
-            print(check)
-        else:
-            return check
-    return get_congratulation(user_name)
-
-
 def start_game():
     user_name: str = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    random_number: int = get_random_number()
-    print(f'Question: {random_number}')
-    user_answer: str = get_user_answer()
-    check: str = check_user_answer(random_number, user_name, user_answer)
-    user_answers: str = get_user_answers(check, user_name)
-    print(user_answers)
+    count = 0
+    while count < 3:
+        random_number: int = get_random_number()
+        print(f'Question: {random_number}')
+        user_answer: str = get_user_answer()
+        check: str = check_user_answer(random_number, user_name, user_answer)
+        if check == 'Correct!':
+            count += 1
+            print(check)
+        else:
+            return print(check)
+    print(get_congratulation(user_name))
