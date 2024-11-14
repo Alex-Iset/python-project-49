f1e909ec61cc78685345575fde5dde5494ceae4c
Alex Iset <fl3shtrader@yandex.ru>
2024-11-14 13:13:17 +0300

    fixed: small fixes

--- brain_games/game_brain_even.py
+++ brain_games/game_brain_even.py
@@ -4,7 +4,9 @@ import prompt
 
 def get_random_number() -> int:
     """Returns a random number in the range from 1 to 100"""
-    random_number: int = randint(1, 100)
+    start_range = 1
+    end_range = 100
+    random_number: int = randint(start_range, end_range)
     return random_number
 
 
@@ -16,7 +18,8 @@ def get_user_answer() -> str:
 
 def get_congratulation(user_name: str) -> str:
     """Congratulates the user"""
-    return f'Congratulations, {user_name}!'
+    congratulation = f'Congratulations, {user_name}!'
+    return congratulation
 
 
 def check_user_answer(random_number: int,
@@ -27,28 +30,30 @@ def check_user_answer(random_number: int,
         if random_number % 2 == 0:
             return 'Correct!'
         else:
-            return (f"'yes' is wrong answer ;(. "
-                    f"Correct answer was 'no'."
-                    f"\nLet's try again, {user_name}!")
+            return (f'"yes" is wrong answer ;(. '
+                    f'Correct answer was "no".'
+                    f'\nLet\'s try again, {user_name}!')
     elif user_answer.lower() == 'no':
         if random_number % 2 != 0:
             return 'Correct!'
         else:
-            return (f"'no' is wrong answer ;(. "
-                    f"Correct answer was 'yes'."
-                    f"\nLet's try again, {user_name}!")
+            return (f'"no" is wrong answer ;(. '
+                    f'Correct answer was "yes".'
+                    f'\nLet\'s try again, {user_name}!')
     else:
-        return (f"'{user_answer}' is wrong answer ;(. "
-                f"Correct answer was 'yes' or 'no'."
-                f"\nLet's try again, {user_name}!")
+        return (f'"{user_answer}" is wrong answer ;(. '
+                f'Correct answer was "yes" or "no".'
+                f'\nLet\'s try again, {user_name}!')
 
 
 def start_game():
+    """Starts the game"""
     user_name: str = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
     count = 0
-    while count < 3:
+    number_questions = 3
+    while count < number_questions:
         random_number: int = get_random_number()
         print(f'Question: {random_number}')
         user_answer: str = get_user_answer()
