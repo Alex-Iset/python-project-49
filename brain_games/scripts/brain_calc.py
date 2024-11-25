ed997c0bdd8d0f21a5505cc628b5ba94b209f43f
Alex Iset <fl3shtrader@yandex.ru>
2024-11-25 12:30:23 +0300

    added: new script for game "brain_calc"

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.game_brain_calc import game_calc
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    game_calc()
+
+
+if __name__ == '__main__':
+    main()
