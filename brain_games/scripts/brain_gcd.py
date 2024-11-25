30de3fbdde77ebecd57623d33253d1fbb41d06f7
Alex Iset <fl3shtrader@yandex.ru>
2024-11-25 12:31:51 +0300

    added: new scripts for game "brain_gcd"

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.game_brain_gcd import game_gcd
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    game_gcd()
+
+
+if __name__ == '__main__':
+    main()
