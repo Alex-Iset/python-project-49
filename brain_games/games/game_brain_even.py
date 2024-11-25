793be35235e7339c23ce30bbf192b53a918f560c
Alex Iset <fl3shtrader@yandex.ru>
2024-11-25 12:14:34 +0300

    fixed: "games/game_brain_even.py", "scripts/brain_even.py", "pyproject.toml"

--- brain_games/games/game_brain_even.py
+++ brain_games/games/game_brain_even.py
@@ -1,66 +1,24 @@
-from random import randint
-import prompt
+from brain_games.consts_and_logic.game_logic import (
+    start_game, get_random_numbers
+)
+from brain_games.consts_and_logic.game_constants import (
+    CONDITION_GAME_EVEN
+)
 
 
-def get_random_number() -> int:
-    """Returns a random number in the range from 1 to 100"""
-    start_range = 1
-    end_range = 100
-    random_number: int = randint(start_range, end_range)
-    return random_number
+def is_even(random_number: int) -> bool:
+    """Checks the parity of the number and returns a Boolean value"""
+    return True if random_number % 2 == 0 else False
 
 
-def get_user_answer() -> str:
-    """Returns the user's response"""
-    user_answer: str = prompt.string('Your answer: ')
-    return user_answer
+def get_random_number_and_check_even() -> tuple[int, str]:
+    """Checks the parity of a random number and
+    returns it and the result of the check"""
+    random_number = get_random_numbers()
+    check_even = 'yes' if is_even(random_number) else 'no'
+    return random_number, check_even
 
 
-def get_congratulation(user_name: str) -> str:
-    """Congratulates the user"""
-    congratulation = f'Congratulations, {user_name}!'
-    return congratulation
-
-
-def check_user_answer(random_number: int,
-                      user_name: str,
-                      user_answer: str) -> str:
-    """Checks the user's response"""
-    if user_answer.lower() == 'yes':
-        if random_number % 2 == 0:
-            return 'Correct!'
-        else:
-            return (f'"yes" is wrong answer ;(. '
-                    f'Correct answer was "no".'
-                    f'\nLet\'s try again, {user_name}!')
-    elif user_answer.lower() == 'no':
-        if random_number % 2 != 0:
-            return 'Correct!'
-        else:
-            return (f'"no" is wrong answer ;(. '
-                    f'Correct answer was "yes".'
-                    f'\nLet\'s try again, {user_name}!')
-    else:
-        return (f'"{user_answer}" is wrong answer ;(. '
-                f'Correct answer was "yes" or "no".'
-                f'\nLet\'s try again, {user_name}!')
-
-
-def start_game():
+def game_even():
     """Starts the game"""
-    user_name: str = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    count = 0
-    number_questions = 3
-    while count < number_questions:
-        random_number: int = get_random_number()
-        print(f'Question: {random_number}')
-        user_answer: str = get_user_answer()
-        check: str = check_user_answer(random_number, user_name, user_answer)
-        if check == 'Correct!':
-            count += 1
-            print(check)
-        else:
-            return print(check)
-    print(get_congratulation(user_name))
+    start_game(get_random_number_and_check_even, CONDITION_GAME_EVEN)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.game_brain_even import start_game
+from brain_games.games.game_brain_even import game_even
 
 
 def main():
     print('Welcome to the Brain Games!')
-    start_game()
+    game_even()
 
 
 if __name__ == '__main__':
