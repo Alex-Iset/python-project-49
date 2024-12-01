1ecfbc415428343fa80f87ac5123fc58671e6a08
Alex Iset <fl3shtrader@yandex.ru>
2024-12-01 13:09:46 +0300

    fixed: цикл while заменен на цикл for

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,16 +9,15 @@ def start_game(data: callable, condition: str):
     """Starts the game"""
     print('Welcome to the Brain Games!')
     user_name: str = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print(condition)
-    question_number: int = 1
-    while question_number <= NUMBER_QUESTIONS:
+    print(f'Hello, {user_name}!'
+          f'\n{condition}')
+
+    for _ in range(NUMBER_QUESTIONS):
         question, answer = data()
         print(f'Question: {question}')
         user_answer: str = prompt.string('Your answer: ')
         if user_answer.lower() == answer:
             print('Correct!')
-            question_number += 1
         else:
             return print(f'"{user_answer}" is wrong answer ;(. '
                          f'Correct answer was "{answer}".'
