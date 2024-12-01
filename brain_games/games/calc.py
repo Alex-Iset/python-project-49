7dfc9918f45b900d5171665eda9b9ccaa470f026
Alex Iset <fl3shtrader@yandex.ru>
2024-12-01 13:32:51 +0300

    fixed: скорректирована логика игры

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,31 +5,29 @@ from brain_games.utils import get_random_numbers
 from brain_games.constants import CONDITION_GAME_CALC
 
 
-def get_random_operation_and_result() -> tuple[str, str]:
-    """Returns a random arithmetic operation and
-    the result of its calculation"""
+def get_random_math_sign_and_result(
+        number1: int, number2: int
+) -> tuple[str, int]:
+    """Returns a random math sign and the result
+    of the performed operation of two random numbers"""
+    return choice([
+        ('+', number1 + number2),
+        ('-', number1 - number2),
+        ('*', number1 * number2)
+    ])
+
+
+def get_math_operation_and_answer() -> tuple[str, str]:
+    """Returns a math operation and the answer"""
     number1, number2 = (
         get_random_numbers(end_range=20),
         get_random_numbers(end_range=20)
     )
-    random_operation = choice(
-        [
-            f'{number1} + {number2}',
-            f'{number1} - {number2}',
-            f'{number1} * {number2}'
-        ]
-    )
-    split_operation = random_operation.split()
-    if '+' in split_operation:
-        result = str(int(split_operation[0]) + int(split_operation[2]))
-    elif '-' in split_operation:
-        result = str(int(split_operation[0]) - int(split_operation[2]))
-    else:
-        result = str(int(split_operation[0]) * int(split_operation[2]))
-
-    return random_operation, result
+    sign, answer = get_random_math_sign_and_result(number1, number2)
+    math_operation = f'{number1} {sign} {number2}'
+    return math_operation, answer
 
 
 def game_calc():
     """Starts the game"""
-    start_game(get_random_operation_and_result, CONDITION_GAME_CALC)
+    start_game(get_math_operation_and_answer, CONDITION_GAME_CALC)
