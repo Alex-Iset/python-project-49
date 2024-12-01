8bd4d96f6755403837c9081e13ebddaf941dd16a
Alex Iset <fl3shtrader@yandex.ru>
2024-12-01 13:40:03 +0300

    fixed: немного скорректирована логика игры

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,3 +1,5 @@
+from math import gcd
+
 from brain_games.engine import start_game
 from brain_games.utils import get_random_numbers
 from brain_games.constants import CONDITION_GAME_GCD
@@ -9,10 +11,7 @@ def get_random_numbers_and_gcd() -> tuple[str, str]:
         get_random_numbers(end_range=30),
         get_random_numbers(end_range=30)
     )
-    greatest_common_divisor = ''
-    for i in range(1, 30):
-        if number1 % i == 0 == number2 % i == 0:
-            greatest_common_divisor = str(i)
+    greatest_common_divisor = gcd(number1, number2)
     numbers = f'{number1} {number2}'
     return numbers, greatest_common_divisor
 
